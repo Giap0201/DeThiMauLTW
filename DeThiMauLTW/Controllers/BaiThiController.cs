9b586a7f1799412468deeb8fb7e0241f5194b1f0
Nguyễn Hữu Giáp <giap13022005@gmail.com>
2025-12-03 22:34:56 +0700

    Add project files.

---
+++ DeThiMauLTW/Controllers/BaiThiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DeThiMauLTW.Controllers
+{
+    public class BaiThiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
